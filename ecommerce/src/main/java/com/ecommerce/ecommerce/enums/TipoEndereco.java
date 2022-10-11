d03e9aba9430878202744e49338821905d4b0de2
rafael <rafaelribeiroramos96@gmail.com>
2022-10-10 21:44:05 -0300

    Criando usuario acesso

---
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/enums/TipoEndereco.java
@@ -0,0 +1,23 @@
+package com.ecommerce.ecommerce.enums;
+
+public enum TipoEndereco {
+	
+	COBRANCA("Cobranca"),
+	ENTREGA("Entrega");
+	
+	private String descricao;
+	
+	private TipoEndereco(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+
+}
